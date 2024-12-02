620d4e7e14fdab1775d79ca3dfbff8fde4406cc7
Osama Samy <149599819+Osama-Samy@users.noreply.github.com>
2024-12-02 21:30:13 +0200

    first commit

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 import express from 'express'
 import urlRouter from './Routes/url.routes.js'
 import { connectDB } from './Database/dbConn.js'
-
+import cors from 'cors'
 const app = express()
 
 app.use(express.json())
 
+app.use(cors())
 app.use('/', urlRouter)
 
 app.use('*', (req, res) => {
