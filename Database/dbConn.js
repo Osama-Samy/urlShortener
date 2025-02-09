0bcdf50707c6c7aa7a51c365788b014a7da3ec37
Osama Samy <149599819+Osama-Samy@users.noreply.github.com>
2025-02-09 14:18:54 +0200

    Update dbConn.js

--- Database/dbConn.js
+++ Database/dbConn.js
@@ -1,6 +1,7 @@
 import mongoose from 'mongoose'
 
-export const connectDB = mongoose.connect('mongodb://localhost:27017/url-Shortener').then(() => {
+export const connectDB = mongoose.connect("mongodb://ososos:tDEAbbTwWCZt1qQL@cluster0-shard-00-00.cwva7.mongodb.net:27017,cluster0-shard-00-01.cwva7.mongodb.net:27017,cluster0-shard-00-02.cwva7.mongodb.net:27017/?ssl=true&replicaSet=atlas-7byo7a-shard-0&authSource=admin&retryWrites=true&w=majority&appName=dubbingMasterAi"
+).then(() => {
     console.log('Connected successfully to MongoDB')
 }).catch(() => {
     console.log('Error connecting to MongoDB')
